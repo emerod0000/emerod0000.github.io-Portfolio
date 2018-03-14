901dae55551c02f78a32a73a256899badcf56ab8
emerod0000 <36543780+emerod0000@users.noreply.github.com>
2018-03-14 22:51:08 +0000

    Delete showhide.js

--- showhide.js
+++
@@ -1,8 +0,0 @@
-var buttons = document.querySelectorAll('.read-more');
-
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function(event) {
-    var span = event.target.previousElementSibling.querySelector('span');
-    span.classList.add('fade-in');
-  });
-}
