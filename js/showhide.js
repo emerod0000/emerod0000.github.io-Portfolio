9721a27412cf0b02eabf0a0c9d6fa7958dc9347f
emerod0000 <36543780+emerod0000@users.noreply.github.com>
2018-03-14 22:42:35 +0000

    Add files via upload

---
+++ js/showhide.js
@@ -0,0 +1,21 @@
+    
+            $(document).ready(function(){
+                var readMoreHtml = $(".read-more").html();
+                var lessText = readMoreHtml.substr(0, 100);
+                if(readMoreHtml.length > 100){
+                    $(".read-more").html(lessText).append("<a href='' class='read-more-link'> Read More</a>");
+                }
+                else{
+                    $(".read-more").html(readMoreHtml);
+                }
+                
+                $("body").on("click", ".read-more-link", function(event){
+                    event.preventDefault();
+                   $(this).parent.(".read-more").html(readMoreHtml).append("<a href='' class='show-less-link> Read Less</a>")
+                });
+                $("body").on("click", ".show-less-link", function(event){
+                    event.preventDefault();
+                   $(this).parent.(".read-more").html(readMoreHtml.substr(0, 100)).append("<a href='' class='read-more-link> Read More</a>")
+                });
+            });
+            
