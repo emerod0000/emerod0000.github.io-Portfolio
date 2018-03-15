3557fb3672a9841f3258673c38eb8d5a54b70a37
emerod0000 <36543780+emerod0000@users.noreply.github.com>
2018-03-15 01:14:07 +0000

    Update showhide.js

--- js/showhide.js
+++ js/showhide.js
@@ -1,20 +1,3 @@
     
-            $(document).ready(function(){
-                var readMoreHtml = $(".read-more").html();
-                var lessText = readMoreHtml.substr(0, 100);
-                if(readMoreHtml.length > 100){
-                    $(".read-more").html(lessText);
-                else{
-                    $(".read-more").html(readMoreHtml);
-                }
-                
-                $("body").on("click", ".read-more-link", function(event){
-                     event.preventDefault();
-                   $(this).parent.(".read-more").html(readMoreHtml).append("<a href='' class='show-less-link'> Read Less</a>")
-                });
-                $("body").on("click", ".show-less-link", function(event){
-                    event.preventDefault();
-                   $(this).parent.(".read-more").html(readMoreHtml.substr(0, 100)).append("<a href='' class='read-more-link'> Read More</a>")
-                });
-            });
-            
+            <div>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br>asdasdasdasd<br></div>
+<a id="more" href="#">Read more </a>
