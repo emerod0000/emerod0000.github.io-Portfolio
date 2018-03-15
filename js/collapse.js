63739d93c39c96e48e26c4eaaa329074633d9c75
emerod0000 <36543780+emerod0000@users.noreply.github.com>
2018-03-15 14:43:22 +0000

    Update collapse.js

--- js/collapse.js
+++ js/collapse.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
   $("#demo-1").on("hide.bs.collapse", function(){
-    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open');
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span>Open to read');
   });
   $("#demo-1").on("show.bs.collapse", function(){
     $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Close');
   });
   
    $("#demo-2").on("hide.bs.collapse", function(){
-    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open');
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open to read');
   });
   $("#demo-2").on("show.bs.collapse", function(){
     $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Close');
   });
   
    $("#demo-3").on("hide.bs.collapse", function(){
-    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open');
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open to read');
   });
   $("#demo-3").on("show.bs.collapse", function(){
-    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Close');
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Open to read');
   });
 });
